d941631f6e1fa2c5ad63802128e8ef5b75f3eaa6
Stephen Blum <blum.stephen@gmail.com>
2013-06-09 16:45:38 -0700

    first commit with mostly complete UI.

---
+++ app.js
@@ -0,0 +1,135 @@
+(function(){
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// 
+// Setup Variables and Objects
+// 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+var stocks         = {}
+,   stock_tickers  = PUBNUB.$("stock-tickers")
+,   stock_template = PUBNUB.$("stock-template").innerHTML;
+var pubnub         = PUBNUB.init({
+    windowing     : 1000,
+    subscribe_key : 'demo',
+    publish_key   : 'demo'
+});
+
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// 
+// Main Get Started
+// 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+start_stream(pubnub.uuid().slice(-4));
+start_stream('BIDU,CBS,EA,FB,GOOG,LNKD,MSFT,ORCL,TRI,YHOO,ZNGA');
+
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// 
+// Update a Ticker Value on Screen
+// 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function update_stock( data, env, ticker ) {
+
+    // Add Name
+    data['name'] = ticker;
+
+    // Setup Ticker Display
+    var stock = stocks[ticker] = ticker in stocks &&
+    stocks[ticker] || (function(){
+        var div   = pubnub.create('div')
+        ,   stock = {};
+
+        // Remember Ticker ID
+        data.ticker = data.id = ticker
+        div.innerHTML = pubnub.supplant( stock_template, data );
+
+        // Populate UI
+        stock_tickers.insertBefore( div, first_div(stock_tickers) );
+
+        // Update References
+        stock['box']    = pubnub.$('stock-id-'     + ticker);
+        stock['name']   = pubnub.$('stock-name-'   + ticker);
+        stock['time']   = pubnub.$('stock-time-'   + ticker);
+        stock['price']  = pubnub.$('stock-price-'  + ticker);
+        stock['delta']  = pubnub.$('stock-delta-'  + ticker);
+        stock['perc']   = pubnub.$('stock-perc-'   + ticker);
+        stock['vol']    = pubnub.$('stock-vol-'    + ticker);
+        stock['switch'] = pubnub.$('stock-switch-' + ticker);
+
+        console.log("stock['time']",stock['time']);
+
+        // Add Flipswitch
+        flipswitch( ticker, function( on, off ) {
+            //....
+            console.log( on, off );
+        } );
+
+        // Return References
+        return stock;
+
+    })();
+
+    // Update UIstock
+    update_stock_display( data, stock );
+}
+
+function update_stock_display( data, stock ) {
+    console.log('( data, stock )', data, stock );
+    stock['time'].innerHTML  = data.time;
+    stock['price'].innerHTML = data.price; 
+    stock['delta'].innerHTML = data.delta;
+    stock['perc'].innerHTML  = data.perc;
+    stock['vol'].innerHTML   = data.vol;
+}
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// 
+// Flip Switch ON/OFF
+// 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function flipswitch( id, callback ) {
+    console.log('id',id);
+
+    var ticker        = pubnub.$('stock-id-' + id)
+    ,   ticker_switch = pubnub.$('stock-switch-' + id)
+
+    console.log('ticker',ticker);
+    pubnub.bind( 'mousedown,touchstart', ticker, function() {
+        var on        = pubnub.attr( ticker, 'data-on' )
+        ,   state     = on=='on'
+        ,   on_off    = (state?'off':'on')
+        ,   classname = 'switch-'+ on_off +' switch-animate';
+
+        // Flip For Later Switching
+        pubnub.attr( ticker, 'data-on', on_off )
+
+        // Update UI
+        pubnub.attr( ticker_switch, 'class', classname );
+        ticker_switch.className = classname;
+
+        // Run User Callback
+        callback( state, !state );
+
+        return false;
+    } );
+}
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// 
+// Start and Stop Streams
+// 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function start_stream(id) {
+    pubnub.subscribe({
+        channel : id,
+        message : update_stock
+    })
+}
+
+
+
+
+function first_div(elm) { return elm.getElementsByTagName('div')[0] }
+
+})();
